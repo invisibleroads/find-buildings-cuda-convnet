d15c2d8bce1e5f548f3f9d789d9d8d064399dfbf
Roy Hyunjin Han <rhh@crosscompute.com>
2014-05-12 19:18:29 -0700

    Added script to run all bash tests

---
+++ count_buildings/tests/run.sh
@@ -0,0 +1,6 @@
+bash get_tiles_from_image_test.sh
+bash get_examples_from_points.sh
+bash get_dataset_from_examples.sh
+bash get_arrays_from_image.sh
+bash get_marker_from_dataset.sh
+bash get_predictions_from_arrays.sh
