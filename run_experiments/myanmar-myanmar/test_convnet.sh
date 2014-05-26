14fcc832acf1c670ecf1f4e97be910157290bb3a
Roy Hyunjin Han <rhh@crosscompute.com>
2014-05-26 01:33:01 -0700

    Updated options.cfg

--- run_experiments/myanmar-myanmar/test_convnet.sh
+++ run_experiments/myanmar-myanmar/test_convnet.sh
@@ -7,10 +7,10 @@ get_batches_from_dataset \
     --dataset_folder $OUTPUT_FOLDER/training_dataset \
     --batch_size $BATCH_SIZE
 
-# get_batches_from_arrays \
-    # --target_folder $OUTPUT_FOLDER/$MARKER_MODULE/test_batches \
-    # --arrays_folder $OUTPUT_FOLDER/test_arrays \
-    # --batch_size $BATCH_SIZE
+get_batches_from_arrays \
+    --target_folder $OUTPUT_FOLDER/$MARKER_MODULE/test_batches \
+    --arrays_folder $OUTPUT_FOLDER/test_arrays \
+    --batch_size $BATCH_SIZE
 
-# ccn-train options.cfg
-# ccn-predict options.cfg -f $OUTPUT_FOLDER/$MARKER_MODULE/ConvNet__*
+ccn-train options.cfg
+ccn-predict options.cfg -f $OUTPUT_FOLDER/$MARKER_MODULE/ConvNet__*
