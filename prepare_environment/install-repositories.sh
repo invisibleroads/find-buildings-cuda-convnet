caa73711041f2a84f36b91723feca9174fb3af1d
Roy Hyunjin Han <rhh@crosscompute.com>
2014-05-27 02:39:36 -0700

    Flip order

--- prepare_environment/install-repositories.sh
+++ prepare_environment/install-repositories.sh
@@ -4,10 +4,10 @@ git clone git@github.com:invisibleroads/count-buildings.git
 git clone git@github.com:invisibleroads/noccn.git
 v
 cd ~/Projects/crosscompute
-python setup.py develop
 git checkout -b 0.7 origin/0.7
+python setup.py develop
 cd ~/Projects/count-buildings
 python setup.py develop
 cd ~/Projects/noccn
-python setup.py develop
 git checkout -b patch-1 origin/patch-1
+python setup.py develop
