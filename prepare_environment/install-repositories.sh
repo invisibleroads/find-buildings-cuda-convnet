cbbb1abc6241d99f79fc84541af9a4141c6f4eee
Roy Hyunjin Han <rhh@crosscompute.com>
2014-05-27 02:38:18 -0700

    Get latest branch

--- prepare_environment/install-repositories.sh
+++ prepare_environment/install-repositories.sh
@@ -5,7 +5,9 @@ git clone git@github.com:invisibleroads/noccn.git
 v
 cd ~/Projects/crosscompute
 python setup.py develop
+git checkout -b 0.7 origin/0.7
 cd ~/Projects/count-buildings
 python setup.py develop
 cd ~/Projects/noccn
 python setup.py develop
+git checkout -b patch-1 origin/patch-1
