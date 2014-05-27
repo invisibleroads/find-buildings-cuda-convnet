43c0c5b200df5630e0d0fd40123ffc471479034a
Roy Hyunjin Han <rhh@crosscompute.com>
2014-05-27 09:41:04 -0700

    Updated instructions for installing cuda-convnet

--- prepare_environment/install-convnet.sh
+++ prepare_environment/install-convnet.sh
@@ -1,7 +1,15 @@
+v
+
+cd /tmp
+sudo yum -y install libvdpau-devel
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/331.67/NVIDIA-Linux-x86_64-331.67.run
+sudo bash NVIDIA-Linux-x86_64-331.67.run
+
 cd /tmp
 wget http://developer.download.nvidia.com/compute/cuda/6_0/rel/installers/cuda_6.0.37_linux_64.run
 sudo yum -y install libGLU-devel libX11-devel libXmu-devel
 sudo bash cuda_6.0.37_linux_64.run --override
+sudo reboot
 
 cd ~/.virtualenvs/crosscompute/opt
 git clone https://github.com/dnouri/cuda-convnet.git
@@ -9,5 +17,5 @@ cd cuda-convnet
 sudo yum -y install cmake
 sudo yum -y install perl-Env
 sudo yum -y install atlas-devel
-cmake -DBLAS_INCLUDE_DIRS=/usr/lib64/atlas/libcblas.so .
+cmake -DBLAS_LIBRARIES=/usr/lib64/atlas/libcblas.so  .
 make
