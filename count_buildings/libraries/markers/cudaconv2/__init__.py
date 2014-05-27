eb7061df73559ab2f599a096740b44d4bdb562ea
Roy Hyunjin Han <rhh@crosscompute.com>
2014-05-27 23:56:15 +0000

    Fixed CroppedZeroMeanDataProvider

--- count_buildings/libraries/markers/cudaconv2/__init__.py
+++ count_buildings/libraries/markers/cudaconv2/__init__.py
@@ -90,18 +90,18 @@ class CroppedZeroMeanDataProvider(ZeroMeanDataProvider):
         self.pixel_height, self.pixel_width, self.band_count = self.batch_meta['array_shape']
 
         self.border_size = dp_params['crop_border']
-        self.inner_height = pixel_height - self.border_size * 2
-        self.inner_width = pixel_width - self.border_size * 2
+        self.inner_height = self.pixel_height - self.border_size * 2
+        self.inner_width = self.pixel_width - self.border_size * 2
 
         self.multiview = dp_params['multiview_test'] and test
         self.num_views = 5 * 2
         self.data_mult = self.num_views if self.multiview else 1
 
         for d in self.data_dic:
-            d['labels'] = n.require(n.tile(d['labels'].reshape((1, d['data'].shape[1])), (1, self.data_mult)), requirements='C')
+            d['labels'] = np.require(np.tile(d['labels'].reshape((1, d['data'].shape[1])), (1, self.data_mult)), requirements='C')
 
         self.cropped_data = [
-            n.zeros((self.get_data_dims(), self.data_dic[0]['data'].shape[1] * self.data_mult), dtype=n.single) for x in xrange(2)]
+            np.zeros((self.get_data_dims(), self.data_dic[0]['data'].shape[1] * self.data_mult), dtype=np.single) for x in xrange(2)]
 
         self.batches_generated = 0
         self.data_mean = self.batch_meta['data_mean'].T.reshape((self.band_count, self.pixel_height, self.pixel_width))[
@@ -111,20 +111,20 @@ class CroppedZeroMeanDataProvider(ZeroMeanDataProvider):
         ].reshape((self.get_data_dims(), 1))
 
     def get_next_batch(self):
-        epoch, batchnum, datadic = ZeroMeanDataProvider.get_next_batch(self)
+        epoch, batchnum, (data, labels) = ZeroMeanDataProvider.get_next_batch(self)
 
         cropped = self.cropped_data[self.batches_generated % 2]
 
-        self.__trim_borders(datadic['data'], cropped)
+        self.__trim_borders(data, cropped)
         cropped -= self.data_mean
         self.batches_generated += 1
-        return epoch, batchnum, [cropped, datadic['labels']]
+        return epoch, batchnum, [cropped, labels]
 
     def get_data_dims(self, idx=0):
         return self.inner_height * self.inner_width * self.band_count if idx == 0 else 1
 
     def get_plottable_data(self, data):
-        return n.require((data + self.data_mean).T.reshape(data.shape[1], self.band_count, self.inner_height, self.inner_width).swapaxes(1,3).swapaxes(1,2) / 255.0, dtype=n.single)
+        return np.require((data + self.data_mean).T.reshape(data.shape[1], self.band_count, self.inner_height, self.inner_width).swapaxes(1,3).swapaxes(1,2) / 255.0, dtype=np.single)
 
     def __trim_borders(self, x, target):
         y = x.reshape(self.band_count, self.pixel_height, self.pixel_width, x.shape[1])
@@ -144,9 +144,9 @@ class CroppedZeroMeanDataProvider(ZeroMeanDataProvider):
                 target[:,:] = pic.reshape((self.get_data_dims(), x.shape[1]))
         else:
             for c in xrange(x.shape[1]): # loop over cases
-                startY, startX = nr.randint(0,self.border_size*2 + 1), nr.randint(0,self.border_size*2 + 1)
+                startY, startX = np.random.randint(0,self.border_size*2 + 1), np.random.randint(0,self.border_size*2 + 1)
                 endY, endX = startY + self.inner_height, startX + self.inner_width
                 pic = y[:,startY:endY,startX:endX, c]
-                if nr.randint(2) == 0: # also flip the image with 50% probability
+                if np.random.randint(2) == 0: # also flip the image with 50% probability
                     pic = pic[:,:,::-1]
                 target[:,c] = pic.reshape((self.get_data_dims(),))
