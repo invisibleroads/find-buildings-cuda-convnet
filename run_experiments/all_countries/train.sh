affeabd83aeaab83beba09d7429d4ef71d4b2688
Roy Hyunjin Han <rhh@crosscompute.com>
2014-05-31 15:51:39 +0000

    Move folders

--- run_experiments/all_countries/train.sh
+++ run_experiments/all_countries/train.sh
@@ -1,5 +1,5 @@
 BASE_FOLDER=`pwd`
-# cd $BASE_FOLDER/layers-19pct; ccn-train options.cfg
-cd $BASE_FOLDER/layers-conv-local-11pct/200k/crop3-wc0; ccn-train options.cfg
-cd $BASE_FOLDER/layers-conv-local-11pct/200k/crop3-wc0/dropout-layers1; ccn-train options.cfg
-cd $BASE_FOLDER/layers-conv-local-11pct/200k/crop3-wc0/dropout-layers2; ccn-train options.cfg
+# cd $BASE_FOLDER/layer-parameters/19pct; ccn-train options.cfg
+cd $BASE_FOLDER/layer-parameters/conv-local-11pct/crop3-wc0; ccn-train options.cfg
+cd $BASE_FOLDER/layer-parameters/conv-local-11pct/crop3-wc0/dropout-layers1; ccn-train options.cfg
+cd $BASE_FOLDER/layer-parameters/conv-local-11pct/crop3-wc0/dropout-layers2; ccn-train options.cfg
