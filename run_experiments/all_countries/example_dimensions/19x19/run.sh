07c2597c933f8ee60f1bb9c1d8c1c13b24678ee6
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-01 03:13:13 +0000

    Update all_countries/example_dimensions

--- run_experiments/all_countries/example_dimensions/19x19/run.sh
+++ run_experiments/all_countries/example_dimensions/19x19/run.sh
@@ -5,7 +5,7 @@ OVERLAP_DIMENSIONS=5x5
 BATCH_SIZE=10k
 RANDOM_SEED=myanmar
 
-COUNTRY_IMAGES="
+COUNTRY_IMAGES=${1:-"
 ethiopia0
 mali0
 myanmar0
@@ -13,7 +13,7 @@ senegal0
 tanzania0
 uganda0
 uganda1
-"
+"}
 for COUNTRY_IMAGE in $COUNTRY_IMAGES; do
 echo $COUNTRY_IMAGE
 IMAGE_PATH=~/Links/satellite-images/$COUNTRY_IMAGE
@@ -33,38 +33,38 @@ get_dataset_from_examples \
     --excluded_pixel_bounds $PIXEL_BOUNDS \
     --maximum_dataset_size 110k
 done
-get_batches_from_datasets \
-    --target_folder $OUTPUT_FOLDER/training_batches \
-    --dataset_folders \
-        $OUTPUT_FOLDER/training_dataset/ethiopia0 \
-        $OUTPUT_FOLDER/training_dataset/mali0 \
-        $OUTPUT_FOLDER/training_dataset/myanmar0 \
-        $OUTPUT_FOLDER/training_dataset/senegal0 \
-        $OUTPUT_FOLDER/training_dataset/tanzania0 \
-        $OUTPUT_FOLDER/training_dataset/uganda0 \
-        $OUTPUT_FOLDER/training_dataset/uganda1 \
-    --batch_size $BATCH_SIZE
+# get_batches_from_datasets \
+    # --target_folder $OUTPUT_FOLDER/training_batches \
+    # --dataset_folders \
+        # $OUTPUT_FOLDER/training_dataset/ethiopia0 \
+        # $OUTPUT_FOLDER/training_dataset/mali0 \
+        # $OUTPUT_FOLDER/training_dataset/myanmar0 \
+        # $OUTPUT_FOLDER/training_dataset/senegal0 \
+        # $OUTPUT_FOLDER/training_dataset/tanzania0 \
+        # # $OUTPUT_FOLDER/training_dataset/uganda0 \
+        # $OUTPUT_FOLDER/training_dataset/uganda1 \
+    # --batch_size $BATCH_SIZE
 
-get_batches_from_datasets \
-    --target_folder $OUTPUT_FOLDER/validation_batches \
-    --dataset_folders \
-        $OUTPUT_FOLDER/training_dataset/myanmar0 \
-    --batch_size $BATCH_SIZE \
-    --array_shape 32,32,3
+# get_batches_from_datasets \
+    # --target_folder $OUTPUT_FOLDER/validation_batches \
+    # --dataset_folders \
+        # $OUTPUT_FOLDER/training_dataset/myanmar0 \
+    # --batch_size $BATCH_SIZE \
+    # --array_shape 32,32,3
 
-COUNTRY_IMAGE=myanmar0
-IMAGE_PATH=~/Links/satellite-images/$COUNTRY_IMAGE
-get_arrays_from_image \
-    --target_folder $OUTPUT_FOLDER/test_arrays \
-    --image_path $IMAGE_PATH \
-    --overlap_dimensions $OVERLAP_DIMENSIONS \
-    --tile_dimensions $EXAMPLE_DIMENSIONS \
-    --included_pixel_bounds $PIXEL_BOUNDS
-get_batches_from_arrays \
-    --target_folder $OUTPUT_FOLDER/test_batches \
-    --arrays_folder $OUTPUT_FOLDER/test_arrays \
-    --batch_size $BATCH_SIZE \
-    --array_shape 32,32,3
+# COUNTRY_IMAGE=myanmar0
+# IMAGE_PATH=~/Links/satellite-images/$COUNTRY_IMAGE
+# get_arrays_from_image \
+    # --target_folder $OUTPUT_FOLDER/test_arrays \
+    # --image_path $IMAGE_PATH \
+    # --overlap_dimensions $OVERLAP_DIMENSIONS \
+    # --tile_dimensions $EXAMPLE_DIMENSIONS \
+    # --included_pixel_bounds $PIXEL_BOUNDS
+# get_batches_from_arrays \
+    # --target_folder $OUTPUT_FOLDER/test_batches \
+    # --arrays_folder $OUTPUT_FOLDER/test_arrays \
+    # --batch_size $BATCH_SIZE \
+    # --array_shape 32,32,3
 
 # ccn-train options.cfg
 # ccn-predict options.cfg -f $OUTPUT_FOLDER/ConvNet__*
