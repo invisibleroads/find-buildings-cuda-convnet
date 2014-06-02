6908138fa3d7e7d8ba3bb6458d3fb05d20a2f56e
Roy Hyunjin Han <starsareblueandfaraway@gmail.com>
2014-06-02 04:16:52 -0700

    Add batch_size to get_dataset_from_examples

--- run_experiments/in_production/uganda/run.sh
+++ run_experiments/in_production/uganda/run.sh
@@ -1,4 +1,4 @@
-export OUTPUT_FOLDER=~/Experiments/in_production/`basename $(dirname $(pwd)/$0)`
+OUTPUT_FOLDER=~/Experiments/in_production/`basename $(dirname $(pwd)/$0)`
 EXAMPLE_DIMENSIONS=12x12
 OVERLAP_DIMENSIONS=5x5
 BATCH_SIZE=10k
@@ -18,25 +18,26 @@ get_examples_from_points \
     --image_path $IMAGE_PATH \
     --points_path $POINTS_PATH \
     --example_dimensions $EXAMPLE_DIMENSIONS
-get_dataset_from_examples \
-    --target_folder $OUTPUT_FOLDER/training_dataset_210k/$COUNTRY_IMAGE \
-    --random_seed $RANDOM_SEED \
-    --examples_folder $OUTPUT_FOLDER/examples/$COUNTRY_IMAGE \
-    --maximum_dataset_size 210k
+# get_dataset_from_examples \
+    # --target_folder $OUTPUT_FOLDER/training_dataset_210k/$COUNTRY_IMAGE \
+    # --random_seed $RANDOM_SEED \
+    # --examples_folder $OUTPUT_FOLDER/examples/$COUNTRY_IMAGE \
+    # --maximum_dataset_size 210k
 get_dataset_from_examples \
     --target_folder $OUTPUT_FOLDER/training_dataset/$COUNTRY_IMAGE \
     --random_seed $RANDOM_SEED \
-    --examples_folder $OUTPUT_FOLDER/examples/$COUNTRY_IMAGE
+    --examples_folder $OUTPUT_FOLDER/examples/$COUNTRY_IMAGE \
+    --batch_size $BATCH_SIZE
 done
 
 DATASET_FOLDERS=""
 for COUNTRY_IMAGE in $COUNTRY_IMAGES; do
     DATASET_FOLDERS="$DATASET_FOLDERS $OUTPUT_FOLDER/training_dataset_210k/$COUNTRY_IMAGE"
 done
-get_batches_from_datasets \
-    --target_folder $OUTPUT_FOLDER/training_batches_210k \
-    --dataset_folders $DATASET_FOLDERS \
-    --batch_size $BATCH_SIZE
+# get_batches_from_datasets \
+    # --target_folder $OUTPUT_FOLDER/training_batches_210k \
+    # --dataset_folders $DATASET_FOLDERS \
+    # --batch_size $BATCH_SIZE
 
 DATASET_FOLDERS=""
 for COUNTRY_IMAGE in $COUNTRY_IMAGES; do
