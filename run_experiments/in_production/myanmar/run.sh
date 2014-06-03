d92eebb741b6b9fd6e3210a03733c327caea5093
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-03 12:03:36 +0000

    Fix get_batch_count_from_batches

--- run_experiments/in_production/myanmar/run.sh
+++ run_experiments/in_production/myanmar/run.sh
@@ -16,20 +16,20 @@ POINTS_PATH=~/Links/building-locations/$COUNTRY_IMAGE
     # --image_path $IMAGE_PATH \
     # --points_path $POINTS_PATH \
     # --example_dimensions $EXAMPLE_DIMENSIONS
-get_dataset_from_examples \
-    --target_folder $OUTPUT_FOLDER/training_dataset/$COUNTRY_IMAGE \
-    --random_seed $RANDOM_SEED \
-    --examples_folder $OUTPUT_FOLDER/examples/$COUNTRY_IMAGE \
-    --batch_size $BATCH_SIZE
+# get_dataset_from_examples \
+    # --target_folder $OUTPUT_FOLDER/training_dataset/$COUNTRY_IMAGE \
+    # --random_seed $RANDOM_SEED \
+    # --examples_folder $OUTPUT_FOLDER/examples/$COUNTRY_IMAGE \
+    # --batch_size $BATCH_SIZE
 done
 
 DATASET_FOLDERS=""
 for COUNTRY_IMAGE in $COUNTRY_IMAGES; do
     DATASET_FOLDERS="$DATASET_FOLDERS $OUTPUT_FOLDER/training_dataset/$COUNTRY_IMAGE"
 done
-# get_batches_from_datasets \
-    # --target_folder $OUTPUT_FOLDER/training_batches \
-    # --dataset_folders $DATASET_FOLDERS \
-    # --batch_size $BATCH_SIZE
+get_batches_from_datasets \
+    --target_folder $OUTPUT_FOLDER/training_batches \
+    --dataset_folders $DATASET_FOLDERS \
+    --batch_size $BATCH_SIZE
 
 # ccn-train options.cfg
