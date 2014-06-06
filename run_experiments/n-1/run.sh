5d30c82e094501070ac55a2a7988d79eac41726a
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-06 19:07:36 +0000

    Update run.sh

--- run_experiments/n-1/run.sh
+++ run_experiments/n-1/run.sh
@@ -61,10 +61,6 @@ ccn-train options.cfg \
     --train-range 0-$(($MAX_BATCH_INDEX_MINUS_ONE > 0 ? $MAX_BATCH_INDEX_MINUS_ONE : 0)) \
     --test-range $MAX_BATCH_INDEX \
     2>&1 | tee -a $LOG_PATH
-pushd $OUTPUT_FOLDER
-tar czvf $CLASSIFIER_NAME-batches.tar.gz training_batches
-rm -rf training_batches
-popd
 
 CONVNET_PATH=`ls -d -t -1 $OUTPUT_FOLDER/classifiers/ConvNet__* | head -n 1`
 CLASSIFIER_PATH=$OUTPUT_FOLDER/classifiers/$CLASSIFIER_NAME-n-1
@@ -72,7 +68,7 @@ rm -rf $CLASSIFIER_PATH
 mv $CONVNET_PATH $CLASSIFIER_PATH
 date 2>&1 | tee -a $LOG_PATH
 ccn-predict options.cfg \
-    --write-preds $OUTPUT_FOLDER/predictions.csv \
+    --write-preds $OUTPUT_FOLDER/probabilities.csv \
     --data-path $OUTPUT_FOLDER/training_batches \
     --train-range 0 \
     --test-range $MAX_BATCH_INDEX \
@@ -83,6 +79,10 @@ ARRAY_SHAPE=`
     get_array_shape_from_batches \
         --batches_folder $OUTPUT_FOLDER/training_batches`
 TILE_DIMENSIONS=1000,1000
+pushd $OUTPUT_FOLDER
+tar czvf $CLASSIFIER_NAME-batches.tar.gz training_batches
+rm -rf training_batches
+popd
 for IMAGE_NAME in $IMAGE_NAMES; do
     PIXEL_BOUNDS_LIST=`\
         get_tiles_from_image \
@@ -94,7 +94,7 @@ for IMAGE_NAME in $IMAGE_NAMES; do
     for PIXEL_BOUNDS in $PIXEL_BOUNDS_LIST; do
         date 2>&1 | tee -a $LOG_PATH
         get_arrays_from_image \
-            --target_folder ~/Downloads/$IMAGE_NAME/arrays \
+            --target_folder ~/Downloads/$IMAGE_NAME/arrays-$PIXEL_BOUNDS \
             --image_path ~/Links/satellite-images/$IMAGE_NAME \
             --tile_dimensions $TILE_DIMENSIONS \
             --overlap_dimensions $EXAMPLE_DIMENSIONS \
@@ -103,35 +103,37 @@ for IMAGE_NAME in $IMAGE_NAMES; do
         # --included_pixel_bounds $PIXEL_BOUNDS \
         date 2>&1 | tee -a $LOG_PATH
         get_batches_from_arrays \
-            --target_folder ~/Downloads/$IMAGE_NAME/batches \
+            --target_folder ~/Downloads/$IMAGE_NAME/batches-$PIXEL_BOUNDS \
             --random_seed $RANDOM_SEED \
-            --arrays_folder ~/Downloads/$IMAGE_NAME/arrays \
+            --arrays_folder ~/Downloads/$IMAGE_NAME/arrays-$PIXEL_BOUNDS \
             --batch_size $BATCH_SIZE \
             --array_shape $ARRAY_SHAPE \
             2>&1 | tee -a $LOG_PATH
         pushd ~/Downloads
-        tar czvf $IMAGE_NAME-arrays.tar.gz $IMAGE_NAME/arrays
-        rm -rf $IMAGE_NAME/arrays
+        tar czvf $IMAGE_NAME-arrays-$PIXEL_BOUNDS.tar.gz $IMAGE_NAME/arrays-$PIXEL_BOUNDS
+        rm -rf $IMAGE_NAME/arrays-$PIXEL_BOUNDS
         popd
 
         MAX_BATCH_INDEX=`get_index_from_batches \
-            --batches_folder ~/Downloads/$IMAGE_NAME/batches`
+            --batches_folder ~/Downloads/$IMAGE_NAME/batches-$PIXEL_BOUNDS`
         date 2>&1 | tee -a $LOG_PATH
         ccn-predict options.cfg \
-            --write-preds $OUTPUT_FOLDER/$IMAGE_NAME/predictions.csv \
-            --data-path ~/Downloads/$IMAGE_NAME/batches \
+            --write-preds ~/Downloads/$IMAGE_NAME/probabilities.csv \
+            --data-path ~/Downloads/$IMAGE_NAME/batches-$PIXEL_BOUNDS \
             --train-range 0 \
             --test-range 0-$MAX_BATCH_INDEX \
             -f $CLASSIFIER_PATH \
             2>&1 | tee -a $LOG_PATH
+        cp ~/Downloads/$IMAGE_NAME/probabilities.csv ~/Downloads/$IMAGE_NAME/probabilities-$PIXEL_BOUNDS.csv
         pushd ~/Downloads
-        tar czvf $IMAGE_NAME-batches.tar.gz $IMAGE_NAME/batches
-        rm -rf $IMAGE_NAME/batches
+        tar czvf $IMAGE_NAME-batches-$PIXEL_BOUNDS.tar.gz $IMAGE_NAME/batches-$PIXEL_BOUNDS
+        rm -rf $IMAGE_NAME/batches-$PIXEL_BOUNDS
         popd
 
         date 2>&1 | tee -a $LOG_PATH
         get_counts_from_probabilities \
-            --probabilities_folder $OUTPUT_FOLDER/$IMAGE_NAME/predictions.csv \
+            --target_folder ~/Downloads/$IMAGE_NAME/counts-$PIXEL_BOUNDS \
+            --probabilities_folder ~/Downloads/$IMAGE_NAME \
             --image_path ~/Links/satellite-images/$IMAGE_NAME \
             --points_path ~/Links/building-locations/$IMAGE_NAME \
             2>&1 | tee -a $LOG_PATH
