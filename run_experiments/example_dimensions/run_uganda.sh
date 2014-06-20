45f2e64069b71d110e3044b74ed570a2d6890e81
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-20 04:53:51 +0000

    Tweak experiments

--- run_experiments/example_dimensions/run_uganda.sh
+++ run_experiments/example_dimensions/run_uganda.sh
@@ -36,29 +36,29 @@ for POSITIVE_FRACTION in $POSITIVE_FRACTIONS; do
     MAX_TRAINING_BATCH_INDEX=`get_index_from_batches \
         --batches_folder $OUTPUT_FOLDER/training_batches_$POSITIVE_FRACTION`
     MAX_TRAINING_BATCH_INDEX_MINUS_ONE=$(expr $MAX_TRAINING_BATCH_INDEX - 1)
-    log ccn-train options_uganda.cfg \
-        --save-path $OUTPUT_FOLDER/classifiers \
-        --data-path $OUTPUT_FOLDER/training_batches_$POSITIVE_FRACTION \
-        --train-range 0-$(($MAX_TRAINING_BATCH_INDEX_MINUS_ONE > 0 ? $MAX_TRAINING_BATCH_INDEX_MINUS_ONE : 0)) \
-        --test-range $MAX_TRAINING_BATCH_INDEX
+    # log ccn-train options_uganda.cfg \
+        # --save-path $OUTPUT_FOLDER/classifiers \
+        # --data-path $OUTPUT_FOLDER/training_batches_$POSITIVE_FRACTION \
+        # --train-range 0-$(($MAX_TRAINING_BATCH_INDEX_MINUS_ONE > 0 ? $MAX_TRAINING_BATCH_INDEX_MINUS_ONE : 0)) \
+        # --test-range $MAX_TRAINING_BATCH_INDEX
 
-    CONVNET_PATH=`ls -d -t -1 $OUTPUT_FOLDER/classifiers/ConvNet__* | head -n 1`
-    CLASSIFIER_PATH=$OUTPUT_FOLDER/classifiers/$CLASSIFIER_NAME_$POSITIVE_FRACTION
-    mv $CLASSIFIER_PATH /tmp
-    mv $CONVNET_PATH $CLASSIFIER_PATH
-    log ccn-predict options_uganda.cfg \
-        --write-preds $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION.csv \
-        --data-path $OUTPUT_FOLDER/test_batches \
-        --train-range 0 \
-        --test-range 0-$MAX_TEST_BATCH_INDEX \
-        -f $CLASSIFIER_PATH
-    mkdir $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION
-    mv $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION.csv $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION/probabilities.csv
+    # CONVNET_PATH=`ls -d -t -1 $OUTPUT_FOLDER/classifiers/ConvNet__* | head -n 1`
+    # CLASSIFIER_PATH=$OUTPUT_FOLDER/classifiers/$CLASSIFIER_NAME_$POSITIVE_FRACTION
+    # mv $CLASSIFIER_PATH /tmp
+    # mv $CONVNET_PATH $CLASSIFIER_PATH
+    # log ccn-predict options_uganda.cfg \
+        # --write-preds $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION.csv \
+        # --data-path $OUTPUT_FOLDER/test_batches \
+        # --train-range 0 \
+        # --test-range 0-$MAX_TEST_BATCH_INDEX \
+        # -f $CLASSIFIER_PATH
+    # mkdir $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION
+    # mv $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION.csv $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION/probabilities.csv
     log get_counts_from_probabilities \
         --target_folder $OUTPUT_FOLDER/counts_$POSITIVE_FRACTION \
         --probabilities_folder $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION \
-        --image_path ~/Links/satellite-images/myanmar0 \
-        --points_path ~/Links/building-locations/myanmar0 \
+        --image_path ~/Links/satellite-images/$TEST_IMAGE_NAME \
+        --points_path ~/Links/building-locations/$TEST_IMAGE_NAME \
         --actual_radius $ACTUAL_RADIUS
 
 done
