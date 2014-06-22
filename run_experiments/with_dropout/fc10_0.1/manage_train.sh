872d098043d3219586cf131ac8ff8bedde6d1e56
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-22 08:52:18 -0700

    Save dropout experiments

---
+++ run_experiments/with_dropout/fc10_0.1/manage_train.sh
@@ -0,0 +1,6 @@
+bash train.sh \
+    myanmar \
+    "myanmar0" \
+    10x10 5x5 20,20,4 \
+    myanmar0 13260,2320,14060,2920 \
+    5
---
+++ run_experiments/with_dropout/output_0.1/train.sh
@@ -0,0 +1,56 @@
+CLASSIFIER_NAME=$1
+TRAINING_IMAGE_NAMES=$2
+EXAMPLE_DIMENSIONS=$3
+OVERLAP_DIMENSIONS=$4
+ARRAY_SHAPE=$5
+TEST_IMAGE_NAME=$6
+TEST_PIXEL_BOUNDS=$7
+MINIMUM_RADIUS=$8
+
+RANDOM_SEED=crosscompute
+BATCH_SIZE=1k
+EXPERIMENT_NAME=in_production
+OUTPUT_FOLDER=~/Experiments/$EXPERIMENT_NAME/$CLASSIFIER_NAME
+mkdir -p $OUTPUT_FOLDER
+source ~/Projects/count-buildings/run_experiments/log.sh
+TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
+LOG_PATH=$OUTPUT_FOLDER/`basename $0`-$TIMESTAMP.log
+
+MAX_TEST_BATCH_INDEX=`get_index_from_batches \
+    --batches_folder $OUTPUT_FOLDER/test_batches`
+MAX_TEST_BATCH_INDEX_MINUS_ONE=$(expr $MAX_TEST_BATCH_INDEX - 1)
+
+POSITIVE_FRACTIONS="
+0.010
+"
+for POSITIVE_FRACTION in $POSITIVE_FRACTIONS; do
+
+    MAX_TRAINING_BATCH_INDEX=`get_index_from_batches \
+        --batches_folder $OUTPUT_FOLDER/training_batches_$POSITIVE_FRACTION`
+    MAX_TRAINING_BATCH_INDEX_MINUS_ONE=$(expr $MAX_TRAINING_BATCH_INDEX - 1)
+    log ccn-train options.cfg \
+        --save-path $OUTPUT_FOLDER/classifiers \
+        --data-path $OUTPUT_FOLDER/training_batches_$POSITIVE_FRACTION \
+        --train-range 0-$(($MAX_TRAINING_BATCH_INDEX_MINUS_ONE > 0 ? $MAX_TRAINING_BATCH_INDEX_MINUS_ONE : 0)) \
+        --test-range $MAX_TRAINING_BATCH_INDEX
+
+    CONVNET_PATH=`ls -d -t -1 $OUTPUT_FOLDER/classifiers/ConvNet__* | head -n 1`
+    CLASSIFIER_PATH=$OUTPUT_FOLDER/classifiers/${POSITIVE_FRACTION}_${TIMESTAMP}
+    mv $CONVNET_PATH $CLASSIFIER_PATH
+    log ccn-predict options.cfg \
+        --write-preds $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION.csv \
+        --data-path $OUTPUT_FOLDER/test_batches \
+        --train-range 0 \
+        --test-range 0-$MAX_TEST_BATCH_INDEX \
+        -f $CLASSIFIER_PATH
+
+    mkdir -p $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION
+    mv $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION.csv $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION/probabilities.csv
+    log get_counts_from_probabilities \
+        --target_folder $OUTPUT_FOLDER/counts_$POSITIVE_FRACTION \
+        --probabilities_folder $OUTPUT_FOLDER/probabilities_$POSITIVE_FRACTION \
+        --image_path ~/Links/satellite-images/$TEST_IMAGE_NAME \
+        --points_path ~/Links/building-locations/$TEST_IMAGE_NAME \
+        --minimum_radius $MINIMUM_RADIUS
+
+done
