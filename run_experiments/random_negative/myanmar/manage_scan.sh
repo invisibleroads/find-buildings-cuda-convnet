eafbc82ef5df59d1795d72ffc62e2e29b07d1d4a
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-23 05:15:42 +0000

    Do not force random examples

---
+++ run_experiments/random_negative/myanmar/manage_scan.sh
@@ -0,0 +1,10 @@
+IMAGE_NAMES="
+myanmar0
+"
+for IMAGE_NAME in $IMAGE_NAMES; do
+bash scan.sh \
+    ~/Experiments/in_production/myanmar/classifiers/0.010 \
+    ~/Links/satellite-images/$IMAGE_NAME \
+    ~/Links/building-locations/$IMAGE_NAME \
+    10x10 5x5 20,20,4 10
+done
