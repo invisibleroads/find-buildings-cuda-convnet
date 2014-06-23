66a056fbe2d0e986e51ec9bf0a04f88951812096
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-23 13:54:33 +0000

    Save logs too

--- run_experiments/20140623/save_classifiers.sh
+++ run_experiments/20140623/save_classifiers.sh
@@ -3,3 +3,4 @@ TARGET_PATH=classifiers_${EXPERIMENT_NAME}.tar.gz
 rm $TARGET_PATH
 cd $HOME/Experiments/${EXPERIMENT_NAME}
 find . -name ConvNet__* -exec tar -rvf $TARGET_PATH {} \;
+find . -name *.log -exec tar -rvf $TARGET_PATH {} \;
