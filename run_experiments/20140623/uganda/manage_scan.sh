3a3110ff2d8c9687ccbd0bbfb3e453e06a94c644
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-24 03:29:11 +0000

    Update in_production layers

--- run_experiments/20140623/uganda/manage_scan.sh
+++ run_experiments/20140623/uganda/manage_scan.sh
@@ -1,5 +1,7 @@
 CLASSIFIER_NAMES="
 uganda-20140619-0812
+"
+CLASSIFIER_NAMES="
 uganda-20140622-0131
 uganda-20140623-141751
 uganda-20140623-173736
@@ -15,7 +17,7 @@ export ACTUAL_RADIUS=13.2
 export RANDOM_SEED=crosscompute
 export BATCH_SIZE=5k
 export TILE_DIMENSIONS=1000,1000
-# bash prepare_scan.sh
+bash prepare_scan.sh
 for CLASSIFIER_NAME in $CLASSIFIER_NAMES; do
     export CLASSIFIER_NAME
     export CLASSIFIER_PATH=~/Storage/building-classifiers/$CLASSIFIER_NAME
