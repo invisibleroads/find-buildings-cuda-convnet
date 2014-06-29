6291e97cdd0a8caf0f6c0021e49552462e4bafea
Roy Hyunjin Han <rhh@crosscompute.com>
2014-06-29 18:25:16 +0000

    Save uganda0 experiments

--- run_experiments/20140624-0758/uganda/manage_scan.sh
+++ run_experiments/20140624-0758/uganda/manage_scan.sh
@@ -1,15 +1,14 @@
 CLASSIFIER_NAMES="
-20140624-154605-0.01
-20140624-154605-0.02
+20140626-175008-0.015
 "
 export EXPERIMENT_NAME=`basename $(dirname $(dirname $(pwd)/$0))`
-export IMAGE_NAME=uganda1
+export IMAGE_NAME=uganda0
 export IMAGE_PATH=~/Links/satellite-images/$IMAGE_NAME
 export POINTS_PATH=~/Links/building-locations/$IMAGE_NAME
 export EXAMPLE_DIMENSIONS=19x19
 export OVERLAP_DIMENSIONS=9.5x9.5
 export ARRAY_SHAPE=32,32,3
-export ACTUAL_RADIUS=9.5
+export MINIMUM_RADIUS=9
 export RANDOM_SEED=crosscompute
 export BATCH_SIZE=5k
 export TILE_DIMENSIONS=1000,1000
