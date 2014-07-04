18d3bbcfed578e6c7d7d34c8e07bffe3eb88ab67
Roy Hyunjin Han <rhh@crosscompute.com>
2014-07-04 07:11:08 +0000

    Fix save_classifiers.sh

--- run_experiments/20140630-1739/myanmar/manage_scan.sh
+++ run_experiments/20140630-1739/myanmar/manage_scan.sh
@@ -1,6 +1,5 @@
 CLASSIFIER_NAMES="
-20140624-154552-0.004
-20140624-154552-0.01
+20140630-1739
 "
 export EXPERIMENT_NAME=`basename $(dirname $(dirname $(pwd)/$0))`
 export IMAGE_NAME=myanmar0
@@ -16,6 +15,6 @@ export TILE_DIMENSIONS=1000,1000
 bash prepare_scan.sh
 for CLASSIFIER_NAME in $CLASSIFIER_NAMES; do
     export CLASSIFIER_NAME
-    export CLASSIFIER_PATH=~/Experiments/20140624-0758/myanmar/classifiers/$CLASSIFIER_NAME
+    export CLASSIFIER_PATH=~/Experiments/20140630-1739/myanmar/classifiers/$CLASSIFIER_NAME
     bash scan.sh
 done
