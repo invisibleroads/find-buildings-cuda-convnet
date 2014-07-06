12e01325500d64fe5e61e191651f217daae7e5d7
Roy Hyunjin Han <rhh@crosscompute.com>
2014-07-06 01:59:04 +0000

    Updated scripts

--- run_experiments/20140624-0758/myanmar/manage_scan.sh
+++ run_experiments/20140624-0758/myanmar/manage_scan.sh
@@ -1,6 +1,6 @@
+OUTPUT_NAME="myanmar0"
 CLASSIFIER_NAMES="
-20140628-063031-0.005
-20140628-063031-0.007
+20140705-031924
 "
 export EXPERIMENT_NAME=`basename $(dirname $(dirname $(pwd)/$0))`
 export IMAGE_NAME=myanmar0
@@ -9,13 +9,13 @@ export POINTS_PATH=~/Links/building-locations/$IMAGE_NAME
 export EXAMPLE_DIMENSIONS=16x16
 export OVERLAP_DIMENSIONS=8x8
 export ARRAY_SHAPE=32,32,4
-export ACTUAL_RADIUS=8
+export MINIMUM_RADIUS=8
 export RANDOM_SEED=crosscompute
 export BATCH_SIZE=5k
 export TILE_DIMENSIONS=1000,1000
-bash prepare_scan.sh
+# bash prepare_scan.sh
 for CLASSIFIER_NAME in $CLASSIFIER_NAMES; do
     export CLASSIFIER_NAME
-    export CLASSIFIER_PATH=~/Experiments/20140624-0758/myanmar/classifiers/$CLASSIFIER_NAME
+    export CLASSIFIER_PATH=~/Experiments/20140624-0758/$OUTPUT_NAME/classifiers/$CLASSIFIER_NAME
     bash scan.sh
 done
