22f6510da16a77cbabd2f78579583f131a4c9f32
Roy Hyunjin Han <rhh@crosscompute.com>
2014-07-06 02:51:23 +0000

    Updated scripts

--- run_experiments/20140624-0758/myanmar/manage_scan.sh
+++ run_experiments/20140624-0758/myanmar/manage_scan.sh
@@ -1,9 +1,13 @@
-OUTPUT_NAME="myanmar0"
 CLASSIFIER_NAMES="
 20140705-031924
 "
+export IMAGE_NAMES="
+myanmar1
+myanmar2
+myanmar3
+myanmar4
+"
 export EXPERIMENT_NAME=`basename $(dirname $(dirname $(pwd)/$0))`
-export IMAGE_NAME=myanmar0
 export IMAGE_PATH=~/Links/satellite-images/$IMAGE_NAME
 export POINTS_PATH=~/Links/building-locations/$IMAGE_NAME
 export EXAMPLE_DIMENSIONS=16x16
@@ -13,9 +17,13 @@ export MINIMUM_RADIUS=8
 export RANDOM_SEED=crosscompute
 export BATCH_SIZE=5k
 export TILE_DIMENSIONS=1000,1000
-# bash prepare_scan.sh
-for CLASSIFIER_NAME in $CLASSIFIER_NAMES; do
-    export CLASSIFIER_NAME
-    export CLASSIFIER_PATH=~/Experiments/20140624-0758/$OUTPUT_NAME/classifiers/$CLASSIFIER_NAME
-    bash scan.sh
+for IMAGE_NAME in $IMAGE_NAMES; do
+    bash prepare_scan.sh
+    for CLASSIFIER_NAME in $CLASSIFIER_NAMES; do
+        export CLASSIFIER_NAME
+        export CLASSIFIER_PATH=~/Experiments/20140624-0758/myanmar0/classifiers/$CLASSIFIER_NAME
+        bash scan.sh
+    done
+    rm -rf ~/Downloads/$IMAGE_NAME/arrays-*
+    rm -rf ~/Downloads/$IMAGE_NAME/batches-*
 done
--- run_experiments/20140630-1739/uganda/save_classifiers.sh
+++ run_experiments/20140630-1739/uganda/save_classifiers.sh
@@ -1,8 +1,8 @@
 CLASSIFIER_NAME=`basename $(dirname $(pwd)/$0)`
 EXPERIMENT_NAME=`basename $(dirname $(dirname $(pwd)/$0))`
 TARGET_PATH=classifiers-${CLASSIFIER_NAME}-${EXPERIMENT_NAME}.gz
-rm $TARGET_PATH
 cd $HOME/Experiments/${EXPERIMENT_NAME}
+rm $TARGET_PATH
 find . -name classifiers -exec tar -rvf $TARGET_PATH {} \;
 find . -name *.log -exec tar -rvf $TARGET_PATH {} \;
 find . -name *-counts* -exec tar -rvf $TARGET_PATH {} \;
