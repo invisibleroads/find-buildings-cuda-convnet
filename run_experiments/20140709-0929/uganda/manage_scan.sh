a132f713962b88fa18251f2c5e8e21e3a5f8f537
Roy Hyunjin Han <rhh@crosscompute.com>
2014-07-14 10:32:52 -0700

    Restore uganda

---
+++ run_experiments/20140709-0929/uganda/manage_scan.sh
@@ -0,0 +1,28 @@
+CLASSIFIER_PATHS="
+"
+IMAGE_NAMES="
+uganda0
+uganda1
+"
+export EXPERIMENT_NAME=`basename $(dirname $(dirname $(pwd)/$0))`
+export EXAMPLE_DIMENSIONS=19x19
+export OVERLAP_DIMENSIONS=9.5x9.5
+export ARRAY_SHAPE=32,32,3
+export ACTUAL_RADIUS=9.5
+export RANDOM_SEED=crosscompute
+export BATCH_SIZE=5k
+export TILE_DIMENSIONS=1000,1000
+for IMAGE_NAME in $IMAGE_NAMES; do
+    export IMAGE_NAME
+    export IMAGE_PATH=~/Links/satellite-images/$IMAGE_NAME
+    export POINTS_PATH=~/Links/building-locations/$IMAGE_NAME
+    if [ ! -d ~/Downloads/$IMAGE_NAME/batches-0,0,2000,2000 ]; then
+        bash prepare_scan.sh
+        rm -rf ~/Downloads/$IMAGE_NAME/arrays-*
+    fi
+    for CLASSIFIER_PATH in $CLASSIFIER_PATHS; do
+        export CLASSIFIER_PATH
+        export CLASSIFIER_NAME=`basename $CLASSIFIER_PATH`
+        bash scan.sh
+    done
+done
