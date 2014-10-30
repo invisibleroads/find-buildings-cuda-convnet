d0ac10c7b5b4b3d6e77405cf86739e8fa8dde637
Roy Hyunjin Han <rhh@crosscompute.com>
2014-10-30 18:14:51 -0400

    Wrote normalize_image

--- count_buildings/scripts/normalize_image.py
+++ count_buildings/scripts/normalize_image.py
@@ -35,27 +35,11 @@ def run(
     target_path = join(target_folder, 'image.tif')
     image = SatelliteImage(image_path)
     target_dtype = get_target_dtype(image, target_dtype)
-    zoom_width, zoom_height = get_zoom_dimensions(
+    zoom_dimensions = get_zoom_dimensions(
         image, target_meters_per_pixel_dimensions)
-
-    source_gdal_dataset = image._image
-    target_gdal_dataset = gdal.GetDriverByName('GTiff').Create(
-        target_path,
-        round_number(image.pixel_dimensions[0] * zoom_width),
-        round_number(image.pixel_dimensions[1] * zoom_height),
-        image.band_count, GDAL_DTYPE_BY_ARRAY_DTYPE[target_dtype])
-    target_gdal_dataset.SetGeoTransform(zoom_calibration_pack(
-        image.calibration_pack, (zoom_width, zoom_height)))
-    target_gdal_dataset.SetProjection(source_gdal_dataset.GetProjection())
-
-    for band_number in xrange(1, image.band_count + 1):
-        source_array = source_gdal_dataset.GetRasterBand(
-            band_number).ReadAsArray()
-        lookup_index = get_lookup_index(target_dtype, source_array)
-        target_array = zoom(
-            np.take(lookup_index, source_array), (zoom_height, zoom_width))
-        target_gdal_dataset.GetRasterBand(band_number).WriteArray(target_array)
-    target_gdal_dataset.FlushCache()
+    normalize(target_path, image, target_dtype, zoom_dimensions)
+    return dict(
+        zoom_dimensions=zoom_dimensions)
 
 
 def get_target_dtype(image, target_dtype):
@@ -82,10 +66,44 @@ def get_zoom_dimensions(image, target_meters_per_pixel_dimensions):
     return zoom_width, zoom_height
 
 
-def get_lookup_index(target_dtype, source_array):
-    lookup_index = np.arange(source_array.max() + 1) - source_array.min()
-    lookup_index *= np.iinfo(target_dtype).max / float(lookup_index.max())
-    return lookup_index.astype(target_dtype, copy=False)
+def normalize(target_path, image, target_dtype, (zoom_width, zoom_height)):
+    source_gdal_dataset = image._image
+    target_gdal_dataset = gdal.GetDriverByName('GTiff').Create(
+        target_path,
+        round_number(image.pixel_dimensions[0] * zoom_width),
+        round_number(image.pixel_dimensions[1] * zoom_height),
+        image.band_count, GDAL_DTYPE_BY_ARRAY_DTYPE[target_dtype])
+    target_gdal_dataset.SetGeoTransform(zoom_calibration_pack(
+        image.calibration_pack, (zoom_width, zoom_height)))
+    target_gdal_dataset.SetProjection(source_gdal_dataset.GetProjection())
+    for band_number, band in yield_band(source_gdal_dataset):
+        enhanced_array = enhance_array(band.ReadAsArray(), target_dtype)
+        rescaled_array = zoom(enhanced_array, (zoom_height, zoom_width))
+        target_band = target_gdal_dataset.GetRasterBand(band_number)
+        target_band.WriteArray(rescaled_array)
+    target_gdal_dataset.FlushCache()
+
+
+def yield_band(gdal_dataset):
+    band_count = gdal_dataset.RasterCount
+    for band_number in xrange(1, band_count + 1):
+        yield band_number, gdal_dataset.GetRasterBand(band_number)
+
+
+def enhance_array(source_array, target_dtype):
+    source_min, source_max = source_array.min(), source_array.max()
+    target_min, target_max = get_dtype_bounds(target_dtype)
+    if source_min == target_min and source_max == target_max:
+        return source_array.astype(target_dtype, copy=False)
+    lookup_index = np.arange(source_max + 1) - source_min
+    lookup_index *= target_max / float(lookup_index.max())
+    lookup_index = lookup_index.astype(target_dtype, copy=False)
+    return np.take(lookup_index, source_array)
+
+
+def get_dtype_bounds(dtype):
+    iinfo = np.iinfo(dtype)
+    return iinfo.min, iinfo.max
 
 
 def zoom_calibration_pack(calibration_pack, (zoom_width, zoom_height)):
