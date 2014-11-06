4124f6dcfe0965fe7f38cf9b350bdbe91ea270fa
Roy Hyunjin Han <rhh@crosscompute.com>
2014-11-06 14:12:10 -0500

    Started integrating normalize_image

--- run_experiments/20140724-1114/myanmar/manage_scan.sh
+++ run_experiments/20140724-1114/myanmar/manage_scan.sh
@@ -1,8 +1,9 @@
 cd ~/Projects/count-buildings/run_experiments/20140724-1114/myanmar/
 
 export TARGET_FOLDER=$1
-CLASSIFIER_PATH=$2
-IMAGE_PATH=$3
+export CLASSIFIER_PATH=$2
+export IMAGE_PATH=$3
+export TEMPORARY_FOLDER=`mktemp -d`
 
 export EXAMPLE_METRIC_DIMENSIONS=16x16
 export OVERLAP_METRIC_DIMENSIONS=8x8
@@ -12,11 +13,12 @@ export RANDOM_SEED=crosscompute
 export BATCH_SIZE=5k
 export TILE_METRIC_DIMENSIONS=1000,1000
 
-export IMAGE_PATH
-export IMAGE_NAME=`basename $IMAGE_PATH`
-if [ ! -d ~/Downloads/$IMAGE_NAME/batches-0,0,2000,2000 ]; then
+
+
+
+if [ ! -d $TEMPORARY_FOLDER/batches-* ]; then
     bash prepare_scan.sh
-    rm -rf ~/Downloads/$IMAGE_NAME/arrays-*
+    rm -rf $TEMPORARY_FOLDER/arrays-*
 fi
 
 export CLASSIFIER_PATH
--- run_experiments/20140724-1114/myanmar/scan.sh
+++ run_experiments/20140724-1114/myanmar/scan.sh
@@ -1,35 +1,35 @@
 source ~/Projects/count-buildings/run_experiments/log.sh
-LOG_PATH=~/Downloads/$IMAGE_NAME/`basename $0`-`date +"%Y%m%d-%H%M%S"`.log
-rm ~/Downloads/$IMAGE_NAME/probabilities-*.csv
+LOG_PATH=$TEMPORARY_FOLDER/`basename $0`-`date +"%Y%m%d-%H%M%S"`.log
+rm $TEMPORARY_FOLDER/probabilities-*.csv
 
 PIXEL_BOUNDS_LIST=`\
     get_tiles_from_image \
-        --target_folder ~/Downloads/$IMAGE_NAME/tiles \
+        --target_folder $TEMPORARY_FOLDER/tiles \
         --image_path $IMAGE_PATH \
         --tile_metric_dimensions $TILE_METRIC_DIMENSIONS \
         --overlap_metric_dimensions $EXAMPLE_METRIC_DIMENSIONS \
         --list_pixel_bounds`
 for PIXEL_BOUNDS in $PIXEL_BOUNDS_LIST; do
     MAX_BATCH_INDEX=`get_index_from_batches \
-        --batches_folder ~/Downloads/$IMAGE_NAME/batches-$PIXEL_BOUNDS`
+        --batches_folder $TEMPORARY_FOLDER/batches-$PIXEL_BOUNDS`
     log ccn-predict options.cfg \
-        --write-preds ~/Downloads/$IMAGE_NAME/probabilities-$PIXEL_BOUNDS.csv \
-        --data-path ~/Downloads/$IMAGE_NAME/batches-$PIXEL_BOUNDS \
+        --write-preds $TEMPORARY_FOLDER/probabilities-$PIXEL_BOUNDS.csv \
+        --data-path $TEMPORARY_FOLDER/batches-$PIXEL_BOUNDS \
         --train-range 0 \
         --test-range 0-$MAX_BATCH_INDEX \
         -f $CLASSIFIER_PATH
 done
 
-cat ~/Downloads/$IMAGE_NAME/probabilities-*.csv > \
-    ~/Downloads/$IMAGE_NAME/probabilities.csv
+cat $TEMPORARY_FOLDER/probabilities-*.csv > \
+    $TEMPORARY_FOLDER/probabilities.csv
 sed -i '/0,1,pixel_center_x,pixel_center_y/d' \
-    ~/Downloads/$IMAGE_NAME/probabilities.csv
+    $TEMPORARY_FOLDER/probabilities.csv
 sed -i '1 i 0,1,pixel_center_x,pixel_center_y' \
-    ~/Downloads/$IMAGE_NAME/probabilities.csv
-PROBABILITY_FOLDER=~/Downloads/$IMAGE_NAME/${IMAGE_NAME}-${CLASSIFIER_NAME}-probabilities
+    $TEMPORARY_FOLDER/probabilities.csv
+PROBABILITY_FOLDER=$TEMPORARY_FOLDER/${CLASSIFIER_NAME}-probabilities
 mkdir -p $PROBABILITY_FOLDER
-cp ~/Downloads/$IMAGE_NAME/probabilities.csv $PROBABILITY_FOLDER/probabilities.csv
-COUNTS_FOLDER=~/Downloads/$IMAGE_NAME/${IMAGE_NAME}-${CLASSIFIER_NAME}-counts
+cp $TEMPORARY_FOLDER/probabilities.csv $PROBABILITY_FOLDER/probabilities.csv
+COUNTS_FOLDER=$TEMPORARY_FOLDER/${CLASSIFIER_NAME}-counts
 log get_counts_from_probabilities \
     --target_folder $COUNTS_FOLDER \
     --probabilities_folder $PROBABILITY_FOLDER \
