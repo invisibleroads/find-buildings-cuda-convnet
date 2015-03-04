9809a5b7141030b593a7680551738bd63d45e736
Roy Hyunjin Han <rhh@crosscompute.com>
2015-03-04 17:00:32 -0500

    Update installation instructions

---
+++ prepare_environment/install_cuda.sh
@@ -0,0 +1,11 @@
+# Install kernel 3.12.9
+sudo yum -y install https://kojipkgs.fedoraproject.org//packages/kernel/3.12.9/301.fc20/x86_64/kernel-3.12.9-301.fc20.x86_64.rpm
+sudo yum -y install https://kojipkgs.fedoraproject.org//packages/kernel/3.12.9/301.fc20/x86_64/kernel-devel-3.12.9-301.fc20.x86_64.rpm
+
+# Install cuda dependencies
+sudo yum -y install libvdpau-devel
+
+# Install cuda 6.5
+cd ~/Storage/programs/cuda
+wget http://developer.download.nvidia.com/compute/cuda/6_5/rel/installers/cuda_6.5.14_linux_64.run
+sudo bash cuda_6.5.14_linux_64.run --override
